aa38ea40eaf8dd88bb5371e71781108f202a319a
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-05-30 16:13:56 +0200

    chore(*): init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,13 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         CodeWorker code = new CodeWorker();
-        code.fileInputReadStream();
-        System.out.println(code.testMethod());
     }
 
 
 }
 
 class CodeWorker {
-    public void fileInputReadStream() throws IOException {
-        FileInputStream in = null;
-        FileOutputStream out = null;
-
-        try {
-            in = new FileInputStream("src/input.txt");
-            out = new FileOutputStream("output.txt");
-
-            int c;
-            while ((c = in.read()) != -1) {
-                out.write(c);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (in != null) {
-                in.close();
-            }
-            if (out != null) {
-                out.close();
-            }
-        }
-    }
 
     public String testMethod() {
         return "method";
