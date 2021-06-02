17141ba4a5846288b0fa9cb4b9a0c56e61dbe1e1
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-06-02 21:10:41 +0200

    chore(*): finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,5 +14,11 @@ class CodeWorker {
 
     public String testMethod() {
         return "method";
+    }
+
+    public Integer getAmountOfSips(Integer jumpStrength, List<Integer> hurdles) {
+        var maxHurdle = hurdles.stream().max(Integer::compare).get();
+        var maxSips = maxHurdle - jumpStrength;
+        return maxSips < 0 ? 0 : maxSips;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -15,5 +19,26 @@ class MainTest {
     @Test
     void testMethod() {
         assertEquals(codeWorker.testMethod(), "method");
+    }
+
+    @Test
+    void canJumpWithoutPotion() {
+        Integer jumpStrength = 7;
+        List<Integer> hurdles = new ArrayList<>(Arrays.asList(1,5,2,4,6,2,7));
+        assertEquals(0, codeWorker.getAmountOfSips(jumpStrength, hurdles));
+    }
+
+    @Test
+    void canJumpWithoutPotionWithLowerHurdles() {
+        Integer jumpStrength = 7;
+        List<Integer> hurdles = new ArrayList<>(Arrays.asList(1,5,2,4,6,2));
+        assertEquals(0, codeWorker.getAmountOfSips(jumpStrength, hurdles));
+    }
+
+    @Test
+    void needs2SipsOfPotion() {
+        Integer jumpStrength = 2;
+        List<Integer> hurdles = new ArrayList<>(Arrays.asList(1,2,4,2,3));
+        assertEquals(2, codeWorker.getAmountOfSips(jumpStrength, hurdles));
     }
 }
