7cfa378887f108031d344beec311578a68e15498
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-06-02 21:33:06 +0200

    chore(*): finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         CodeWorker code = new CodeWorker();
@@ -11,5 +13,26 @@ class CodeWorker {
 
     public String testMethod() {
         return "method";
+    }
+
+    public Integer getPositionOfLetter(Character character) {
+        final Integer LETTER_OFFSET = 10;
+        return Character.getNumericValue(character) - LETTER_OFFSET;
+    }
+
+    public Integer getMaxHeightofWord(String word, List<Integer> heights) {
+        var maxHeight = 0;
+        for (String character: word.split("")) {
+            var positionOfLetter = this.getPositionOfLetter(character.charAt(0));
+            var letterHeight = heights.get(positionOfLetter);
+            maxHeight = maxHeight < letterHeight ? letterHeight : maxHeight;
+        }
+        return maxHeight;
+    }
+
+    public Integer getRectanleSize(String word, List<Integer> heights) {
+        Integer LETTER_WIDTH = 1;
+        var maxHeight = this.getMaxHeightofWord(word, heights);
+        return maxHeight * ( word.length() * LETTER_WIDTH );
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -15,5 +19,48 @@ class MainTest {
     @Test
     void testMethod() {
         assertEquals(codeWorker.testMethod(), "method");
+    }
+
+    @Test
+    void getPositionForLetterA() {
+        assertEquals(0, codeWorker.getPositionOfLetter('a'));
+    }
+
+    @Test
+    void getPositionForLetterB() {
+        assertEquals(1, codeWorker.getPositionOfLetter('b'));
+    }
+
+    @Test
+    void getPositionForLetterZ() {
+        assertEquals(25, codeWorker.getPositionOfLetter('z'));
+    }
+
+    @Test
+    void getMaxHeightOfWord7() {
+        String word = "zaba";
+        List<Integer> heights = new ArrayList<>(Arrays.asList(1,3,1,3,1,4,1,3,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,7));
+        assertEquals(7, codeWorker.getMaxHeightofWord(word, heights));
+    }
+
+    @Test
+    void getMaxHeightOfWord3() {
+        String word = "azba";
+        List<Integer> heights = new ArrayList<>(Arrays.asList(1,3,1,3,1,4,1,3,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,2));
+        assertEquals(3, codeWorker.getMaxHeightofWord(word, heights));
+    }
+
+    @Test
+    void getTheRectangleSize28() {
+        String word = "zaba";
+        List<Integer> heights = new ArrayList<>(Arrays.asList(1,3,1,3,1,4,1,3,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,7));
+        assertEquals(28, codeWorker.getRectanleSize(word, heights));
+    }
+
+    @Test
+    void getTheRectangleSize15() {
+        String word = "azbaa";
+        List<Integer> heights = new ArrayList<>(Arrays.asList(1,3,1,3,1,4,1,3,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,2));
+        assertEquals(15, codeWorker.getRectanleSize(word, heights));
     }
 }
