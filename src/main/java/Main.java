5b3b919509f006ce8e4a99968e54221edfe8eb48
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-06-03 21:35:01 +0200

    chore(*): finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,4 +11,25 @@ class CodeWorker {
     public String testMethod() {
         return "method";
     }
+
+    public Integer getSummerGrowth(Integer actualHeight) {
+        return actualHeight + 1;
+    }
+
+    public Integer getSpringGrowth(Integer actualHeight) {
+        return actualHeight * 2;
+    }
+
+    public Integer getCycleGrowth(Integer cycles) {
+        Integer START_HEIGHT = 1;
+        var finalHeightOfTree = START_HEIGHT;
+        for (var i = 0; i < cycles; i++) {
+            if (i%2 != 0) {
+                finalHeightOfTree = this.getSummerGrowth(finalHeightOfTree);
+            } else {
+                finalHeightOfTree = this.getSpringGrowth(finalHeightOfTree);
+            }
+        }
+        return finalHeightOfTree;
+    }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -16,4 +16,58 @@ class MainTest {
     void testMethod() {
         assertEquals(codeWorker.testMethod(), "method");
     }
+
+    @Test
+    void getSummerGrow3() {
+        Integer actualHeight = 2;
+        assertEquals(3, codeWorker.getSummerGrowth(actualHeight));
+    }
+
+    @Test
+    void getSummerGrow4() {
+        Integer actualHeight = 3;
+        assertEquals(4, codeWorker.getSummerGrowth(actualHeight));
+    }
+
+    @Test
+    void getSpringGrow6() {
+        Integer actualHeight = 3;
+        assertEquals(6, codeWorker.getSpringGrowth(actualHeight));
+    }
+
+    @Test
+    void getSpringGrow12() {
+        Integer actualHeight = 6;
+        assertEquals(12, codeWorker.getSpringGrowth(actualHeight));
+    }
+
+    @Test
+    void getGrowthFor1Cycles() {
+        Integer cycles = 1;
+        assertEquals(2, codeWorker.getCycleGrowth(cycles));
+    }
+
+    @Test
+    void getGrowthFor2Cycles() {
+        Integer cycles = 2;
+        assertEquals(3, codeWorker.getCycleGrowth(cycles));
+    }
+
+    @Test
+    void getGrowthFor4Cycles() {
+        Integer cycles = 4;
+        assertEquals(7, codeWorker.getCycleGrowth(cycles));
+    }
+
+    @Test
+    void getGrowthFor5Cycles() {
+        Integer cycles = 5;
+        assertEquals(14, codeWorker.getCycleGrowth(cycles));
+    }
+
+    @Test
+    void getGrowthFor0Cycles() {
+        Integer cycles = 0;
+        assertEquals(1, codeWorker.getCycleGrowth(cycles));
+    }
 }
