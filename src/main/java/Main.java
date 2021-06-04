beea01c99ef31df0b10c731d7e67980600da8168
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-06-04 23:51:15 +0200

    chore(*): extensive test scenarios to validate swapping

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class Main {
     public static void main(String[] args)  {
@@ -10,5 +11,95 @@ public class Main {
 class CodeWorker {
     public String testMethod() {
         return "method";
+    }
+
+    public String testIfPossible(List<List<Integer>> boxes) {
+        var possible = "Possible";
+        var impossible = "Impossible";
+
+        var colorAmount = boxes.size();
+
+        for (var i = 0; i < colorAmount ;i++) {
+            var checkedColor = i;
+            for (var j = 0; j < colorAmount ;j++) {
+                var checkedBox = j;
+                var box = boxes.get(checkedBox);
+                var color = box.get(checkedColor);
+
+                var isNotYetCleanColor = color != 0;
+                if (isNotYetCleanColor) {
+                    for (var q = checkedBox + 1; q < colorAmount; q++) {
+                        if (this.isSwapPossible(checkedColor, box, boxes.get(q))) {
+                            // do swap
+                        }
+                    }
+                }
+            }
+        }
+
+        return possible;
+    }
+
+    public boolean isSwapPossible(Integer color, List<Integer> box1, List<Integer> box2) {
+        var colorAmount = box1.get(color);
+        var initialClear = this.getClearColors(box1, box2);
+        var swapableColors = this.getSwappableColors(color, box1, box2);
+        var swappedBoxes = this.swapColors(color, box1, box2, swapableColors);
+        var afterUpdateClear = this.getClearColors(box1, box2);
+        return afterUpdateClear > initialClear;
+    }
+
+    public List<List<Integer>> swapColors(Integer color, List<Integer> box1, List<Integer> box2, List<List<Integer>> swappable) {
+        var colorToSwap = box1.get(color);
+        for (var colorPair: swappable) {
+            var colorToSwapForAmount = colorPair.get(0);
+            var colorToSwapForPosition = colorPair.get(1);
+
+            if (colorToSwap >= colorToSwapForAmount) {
+                box2.set(colorToSwapForPosition, 0);
+                box2.set(color, box2.get(color) + colorToSwapForAmount);
+                box1.set(color, colorToSwap - colorToSwapForAmount);
+                box1.set(colorToSwapForPosition, box1.get(colorToSwapForPosition) + colorToSwapForAmount);
+                colorToSwap = box1.get(color);
+            } else {
+                var difference = colorToSwapForAmount - colorToSwap;
+                box2.set(colorToSwapForPosition, difference);
+                box2.set(color, box2.get(color) + colorToSwap);
+                box1.set(colorToSwapForPosition, box1.get(colorToSwapForPosition) + colorToSwap);
+                box1.set(color, 0);
+                break;
+            }
+        }
+        return new ArrayList<>(Arrays.asList(box1, box2));
+    }
+
+    public List<List<Integer>> getSwappableColors(Integer color, List<Integer> box1, List<Integer> box2) {
+        List<List<Integer>> colorsToSwap = new ArrayList<>();
+        for (var i = 0; i < box1.size(); i ++) {
+            var colorFrom2ndBox = box2.get(i);
+            if (i != color && colorFrom2ndBox != 0) {
+                var colorFrom1stBox = box1.get(i);
+                if (colorFrom1stBox != 0) {
+                    colorsToSwap.add(new ArrayList<>(Arrays.asList(colorFrom2ndBox, i)));
+                }
+            }
+        }
+        return colorsToSwap;
+    }
+
+    public Integer getClearColors(List<Integer> box1, List<Integer> box2) {
+        var zeroes = 0;
+        for (var color: box1) {
+            if (color == 0) {
+                zeroes++;
+            }
+        }
+
+        for (var color: box2) {
+            if (color == 0) {
+                zeroes++;
+            }
+        }
+        return zeroes;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -15,5 +17,207 @@ class MainTest {
     @Test
     void testMethod() {
         assertEquals(codeWorker.testMethod(), "method");
+    }
+
+    @Test
+    void is2ColorPossible() {
+        List<List<Integer>> list = new ArrayList<>(
+                Arrays.asList(
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        1,1
+                                )
+                        ),
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        1,1
+                                )
+                        )
+                )
+        );
+        assertEquals("Possible", codeWorker.testIfPossible(list));
+    }
+
+    @Test
+    void is2ColorImpossible() {
+        List<List<Integer>> list = new ArrayList<>(
+                Arrays.asList(
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        0,2
+                                )
+                        ),
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        1,1
+                                )
+                        )
+                )
+        );
+        assertEquals("Impossible", codeWorker.testIfPossible(list));
+    }
+
+    @Test
+    void swapColorsWhenSwapperIsBigger() {
+        var swap = codeWorker.swapColors(
+                1,
+                new ArrayList<>(
+                        Arrays.asList(
+                                0,7,4
+                        )
+                ),
+                new ArrayList<>(
+                        Arrays.asList(
+                                5,3,6
+                        )
+                ),
+                new ArrayList<>(
+                        Arrays.asList(
+                                new ArrayList<>(
+                                        Arrays.asList(
+                                                6,2
+                                        )
+                                )
+                        )
+                )
+        );
+        List<List<Integer>> expected = new ArrayList<>(
+                Arrays.asList(
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        0,1,10
+                                )
+                        ),
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        5,9,0
+                                )
+                        )
+                )
+        );
+        assertEquals(expected, swap);
+    }
+
+    @Test
+    void swapColors() {
+        var swap = codeWorker.swapColors(
+                1,
+                new ArrayList<>(
+                        Arrays.asList(
+                                0,7,4
+                        )
+                ),
+                new ArrayList<>(
+                        Arrays.asList(
+                                5,3,8
+                        )
+                ),
+                new ArrayList<>(
+                        Arrays.asList(
+                                new ArrayList<>(
+                                        Arrays.asList(
+                                                8,2
+                                        )
+                                )
+                        )
+                )
+        );
+        List<List<Integer>> expected = new ArrayList<>(
+                Arrays.asList(
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        0,0,11
+                                )
+                        ),
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        5,10,1
+                                )
+                        )
+                )
+        );
+        assertEquals(expected, swap);
+    }
+
+    @Test
+    void doSwap() {
+        var swap = codeWorker.isSwapPossible(
+                1,
+                new ArrayList<>(
+                        Arrays.asList(
+                                0,7,4
+                        )
+                ),
+                new ArrayList<>(
+                        Arrays.asList(
+                                5,3,8
+                        )
+                )
+        );
+
+        assertEquals(true, swap);
+    }
+
+    @Test
+    void getColorsToSwap() {
+        var swap = codeWorker.getSwappableColors(
+                1,
+                new ArrayList<>(
+                        Arrays.asList(
+                                0,7,4
+                        )
+                ),
+                new ArrayList<>(
+                        Arrays.asList(
+                                5,3,8
+                        )
+                )
+        );
+        List<List<Integer>> swappable = new ArrayList<>(
+                Arrays.asList(
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        8,2
+                                )
+                        )
+                )
+        );
+        assertEquals(swappable, swap);
+    }
+
+    @Test
+    void getZeroes() {
+        var clear = codeWorker.getClearColors(
+                new ArrayList<>(
+                        Arrays.asList(
+                                0,7,4
+                        )
+                ),
+                new ArrayList<>(
+                        Arrays.asList(
+                                5,3,8
+                        )
+                )
+        );
+
+        assertEquals(1, clear);
+    }
+
+    @Test
+    void getZeroes4() {
+        var clear = codeWorker.getClearColors(
+                new ArrayList<>(
+                        Arrays.asList(
+                                0,7,0
+                        )
+                ),
+                new ArrayList<>(
+                        Arrays.asList(
+                                5,0,0
+                        )
+                )
+        );
+
+        assertEquals(4, clear);
     }
 }
