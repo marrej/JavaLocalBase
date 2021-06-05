ee12583dd354c7ec92037af4ccc3b448e62a7726
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-06-05 13:30:19 +0200

    chore(*): finished
    -> Thing to note. Simplify. Find the common things between the setts and find the pattern. But always try to simplify

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.math.BigInteger;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args)  {
@@ -9,130 +10,38 @@ public class Main {
 
 }
 
-// NEED TO REWORK EVERYTHING TO WORK WITH BIG INTEGERS, otherwise we are running in to substraction
 class CodeWorker {
 
     public String testIfPossible(List<List<Integer>> boxes) {
         var possible = "Possible";
         var impossible = "Impossible";
 
-        var reducedBoxes = this.reduceBoxes(boxes);
-        if (this.isCorrectlyReduced(reducedBoxes)) {
+        var sizesAndAmounts = this.boxSizesAndBallAmounts(boxes);
+        var boxSizes = sizesAndAmounts.get(0);
+        var ballAmounts = sizesAndAmounts.get(1);
+
+        if (boxSizes.equals(ballAmounts)) {
             return possible;
         } else {
             return impossible;
         }
     }
 
-    public boolean isCorrectlyReduced (List<List<Integer>> boxes) {
-        for (var i = 0; i < boxes.size() ; i ++) {
-            var colorPosition = i;
-            var colorCounter = 0;
-            for (var j = 0; j < boxes.size() ; j ++) {
-                var boxPosition = j;
-                colorCounter += boxes.get(boxPosition).get(colorPosition) > 0 ? 1 : 0;
-                if (colorCounter > 1) {
-                    return false;
-                }
+    public List<List<BigInteger>> boxSizesAndBallAmounts(List<List<Integer>> boxes) {
+        List<BigInteger> sizes = new ArrayList<>();
+        List<BigInteger> amounts = new ArrayList<>();
+        for (var i = 0; i < boxes.size(); i++) {
+            var boxSize = new BigInteger("0");
+            var ballAmount = new BigInteger("0");
+            for (var j = 0; j < boxes.size(); j++) {
+                boxSize = boxSize.add(BigInteger.valueOf(boxes.get(i).get(j)));
+                ballAmount = ballAmount.add(BigInteger.valueOf(boxes.get(j).get(i)));
             }
+            sizes.add(boxSize);
+            amounts.add(ballAmount);
         }
-        return true;
-    }
-
-    public List<List<Integer>> reduceBoxes(List<List<Integer>> boxes) {
-        var colorAmount = boxes.size();
-
-        for (var i = 0; i < colorAmount ;i++) {
-            var checkedColor = i;
-            for (var j = 0; j < colorAmount ;j++) {
-                var checkedBox = j;
-                var box = boxes.get(checkedBox);
-                var color = box.get(checkedColor);
-
-                var isNotYetCleanColor = color != 0;
-                if (isNotYetCleanColor) {
-                    for (var q = checkedBox + 1; q < colorAmount; q++) {
-                        var nextBox = boxes.get(q);
-                        if (this.isSwapPossible(checkedColor, box, nextBox)) {
-                            var swapped = this.getColorsToSwapAndProceed(checkedColor, box, nextBox);
-                            boxes.set(checkedBox, swapped.get(0));
-                            boxes.set(q, swapped.get(1));
-                        }
-                    }
-                }
-            }
-        }
-
-        return boxes;
-    }
-
-    public boolean isSwapPossible(Integer color, List<Integer> box1, List<Integer> box2) {
-        var colorAmount = box1.get(color);
-        var initialClear = this.getClearColors(box1, box2);
-        var swappedBoxes = this.getColorsToSwapAndProceed(color, box1, box2);
-        var afterUpdateClear = this.getClearColors(swappedBoxes.get(0), swappedBoxes.get(1));
-        return afterUpdateClear > initialClear;
-    }
-
-    public List<List<Integer>> getColorsToSwapAndProceed(Integer color, List<Integer> box1, List<Integer> box2) {
-        var swapableColors = this.getSwappableColors(color, box1, box2);
-        return this.swapColors(color, box1, box2, swapableColors);
-    }
-
-    public List<List<Integer>> swapColors(Integer color, List<Integer> box1, List<Integer> box2, List<List<Integer>> swappable) {
-        var colorToSwap = box1.get(color);
-        for (var colorPair: swappable) {
-            var colorToSwapForAmount = colorPair.get(0);
-            var colorToSwapForPosition = colorPair.get(1);
-
-            if (colorToSwap >= colorToSwapForAmount) {
-                box2.set(colorToSwapForPosition, 0);
-                box2.set(color, box2.get(color) + colorToSwapForAmount);
-                box1.set(color, colorToSwap - colorToSwapForAmount);
-                box1.set(colorToSwapForPosition, box1.get(colorToSwapForPosition) + colorToSwapForAmount);
-                colorToSwap = box1.get(color);
-            } else {
-                var difference = colorToSwapForAmount - colorToSwap;
-                box2.set(colorToSwapForPosition, difference);
-                box2.set(color, box2.get(color) + colorToSwap);
-                box1.set(colorToSwapForPosition, box1.get(colorToSwapForPosition) + colorToSwap);
-                box1.set(color, 0);
-                break;
-            }
-        }
-        return new ArrayList<>(Arrays.asList(box1, box2));
-    }
-
-    public List<List<Integer>> getSwappableColors(Integer color, List<Integer> box1, List<Integer> box2) {
-        List<List<Integer>> colorsToSwap = new ArrayList<>();
-        for (var i = 0; i < box1.size(); i ++) {
-            var colorFrom2ndBox = box2.get(i);
-            if (i != color && colorFrom2ndBox != 0) {
-                var colorFrom1stBox = box1.get(i);
-                if (colorFrom1stBox != 0) {
-                    colorsToSwap.add(new ArrayList<>(Arrays.asList(colorFrom2ndBox, i)));
-                } else if (box1.get(color).equals(colorFrom2ndBox) && box2.get(color) != 0) {
-                    colorsToSwap.add(new ArrayList<>(Arrays.asList(colorFrom2ndBox, i)));
-                    break;
-                }
-            }
-        }
-        return colorsToSwap;
-    }
-
-    public Integer getClearColors(List<Integer> box1, List<Integer> box2) {
-        var zeroes = 0;
-        for (var color: box1) {
-            if (color == 0) {
-                zeroes++;
-            }
-        }
-
-        for (var color: box2) {
-            if (color == 0) {
-                zeroes++;
-            }
-        }
-        return zeroes;
+        var sortedSizes = sizes.stream().sorted(BigInteger::compareTo).collect(Collectors.toList());
+        var sortedAmounts = amounts.stream().sorted(BigInteger::compareTo).collect(Collectors.toList());
+        return new ArrayList<>(Arrays.asList(sortedSizes, sortedAmounts));
     }
 }
