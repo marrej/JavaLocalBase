e732210c323415e9e46dedfbc835f71c6c690543
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-06-05 15:29:27 +0200

    chore(*): finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class Main {
     public static void main(String[] args)  {
@@ -10,5 +11,104 @@ public class Main {
 class CodeWorker {
     public String testMethod() {
         return "method";
+    }
+
+    public String encrypt(String input) {
+        var strip = this.strip(input);
+        var rowAndColumn = this.getRowAndColumn(strip);
+        var encryptionMatrix = this.getEncryptMatrix(strip,rowAndColumn);
+        return this.getStringFromEncryptionMatrix(encryptionMatrix, rowAndColumn);
+    }
+
+    public String getStringFromEncryptionMatrix(List<List<String>> encryptionMatrix, List<Integer> rowAndColumn) {
+        var finalString = "";
+        var row = rowAndColumn.get(0);
+        var col = rowAndColumn.get(1);
+
+        for (var i = 0; i < col ;i ++) {
+            var partialString = "";
+            for (var j = 0; j < row; j ++) {
+                var rowList = encryptionMatrix.get(j);
+                if (rowList.size() > i) {
+                    partialString += rowList.get(i);
+                } else {
+                    break;
+                }
+            }
+            if (i != 0) {
+                partialString = " " + partialString;
+            }
+            finalString += partialString;
+        }
+
+        return finalString;
+    }
+
+    public List<List<String>> getEncryptMatrix(String input, List<Integer> rowAndColumn) {
+        List<List<String>> encryptMatrix = new ArrayList<>();
+        var row = rowAndColumn.get(0);
+        var col = rowAndColumn.get(1);
+        var split = input.split("");
+        var counter = 0;
+        for (var i = 0; i < row; i ++) {
+            var column = new ArrayList<String>();
+            for (var j = 0; j < col; j ++) {
+                if (split.length > counter) {
+                    column.add(split[counter]);
+                    counter++;
+                }
+            }
+            encryptMatrix.add(column);
+        }
+        return encryptMatrix;
+    }
+
+    public String strip(String input) {
+        return input.replaceAll(" ", "");
+    }
+
+    public List<Integer> getRowAndColumn(String input) {
+        var length = input.length();
+        var root = Math.sqrt(length);
+        var lowRoot = (int) Math.floor(root);
+        var highRoot = (int) Math.ceil(root);
+        return this.getSmallestArea(this.getCombinationsBiggerOrEqualThanStringLength(length, lowRoot, highRoot));
+    }
+
+    public List<List<Integer>> getCombinationsBiggerOrEqualThanStringLength(Integer strLength, Integer low, Integer high) {
+        List<List<Integer>> finalCombinations = new ArrayList<>();
+        List<List<Integer>> areas = new ArrayList<>(Arrays.asList(
+                new ArrayList<>(Arrays.asList(low,low)),
+                new ArrayList<>(Arrays.asList(low,high)),
+                new ArrayList<>(Arrays.asList(high,high))));
+        for (var area: areas) {
+            var sum = area.stream().reduce(1, (a, b) -> a * b);
+            if (sum >= strLength) {
+                finalCombinations.add(area);
+            }
+        }
+
+        return finalCombinations;
+    }
+
+    public List<Integer> getSmallestArea(List<List<Integer>> combinations) {
+        List<Integer> finalCombination = new ArrayList<>();
+        var smallestSum = -1;
+        for (var combination: combinations) {
+            var sum = 1;
+            for (var option: combination) {
+                sum*=option;
+            }
+            if (smallestSum == -1) {
+                finalCombination = combination;
+                smallestSum = sum;
+            } else {
+                if (sum < smallestSum) {
+                    finalCombination = combination;
+                    smallestSum = sum;
+                }
+            }
+        }
+        return  finalCombination;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -13,7 +15,105 @@ class MainTest {
     }
 
     @Test
+    void returnEncryptedString() {
+        var input = "have a nice day";
+        var output = "hae and via ecy";
+        assertEquals(output, codeWorker.encrypt(input));
+    }
+
+    @Test
+    void returnEncryptedString2() {
+        var input = "feedthedog";
+        var output = "fto ehg ee dd";
+        assertEquals(output, codeWorker.encrypt(input));
+    }
+
+    @Test
+    void returnEncryptedString3() {
+        var input = "feedthedo";
+        var output = "fde etd eho";
+        assertEquals(output, codeWorker.encrypt(input));
+    }
+
+    @Test
     void testMethod() {
         assertEquals(codeWorker.testMethod(), "method");
+    }
+
+    @Test
+    void stripSpaces() {
+        String input = "and it done";
+        String output = "anditdone";
+        assertEquals(output, codeWorker.strip(input));
+    }
+
+    @Test
+    void getRowAndColSize() {
+        String input = "anditdone";
+        List<Integer> rowAndCol = new ArrayList<>(Arrays.asList(
+                3,3
+        ));
+        assertEquals(rowAndCol, codeWorker.getRowAndColumn(input));
+    }
+
+    @Test
+    void getRowAndColSize2nd() {
+        String input = "anditdoneasdfga";
+        List<Integer> rowAndCol = new ArrayList<>(Arrays.asList(
+                4,4
+        ));
+        assertEquals(rowAndCol, codeWorker.getRowAndColumn(input));
+    }
+
+    @Test
+    void getRowAndColSize3rd() {
+        String input = "anditdoneasdf";
+        List<Integer> rowAndCol = new ArrayList<>(Arrays.asList(
+                4,4
+        ));
+        assertEquals(rowAndCol, codeWorker.getRowAndColumn(input));
+    }
+
+    @Test
+    void getRowAndColSize4th() {
+        String input = "anditdoneasd";
+        List<Integer> rowAndCol = new ArrayList<>(Arrays.asList(
+                3,4
+        ));
+        assertEquals(rowAndCol, codeWorker.getRowAndColumn(input));
+    }
+
+    @Test
+    void rowXcolHasToBeEqualOrBiggerThanTheStrippedString() {
+        Integer stringLength = 11;
+        Integer lowRoot = 3;
+        Integer highRoot = 4;
+        List<List<Integer>> roots = new ArrayList<>(Arrays.asList(
+                new ArrayList<>(Arrays.asList(3,4)),
+                new ArrayList<>(Arrays.asList(4,4))
+        ));
+        assertEquals(roots, codeWorker.getCombinationsBiggerOrEqualThanStringLength(11,3,4));
+    }
+
+    @Test
+    void ifMultipleColRowConfigurationsAreFoundUseTheOneWithSmallestArea() {
+        List<List<Integer>> combinations = new ArrayList<>(Arrays.asList(
+                new ArrayList<>(Arrays.asList(3,4)),
+                new ArrayList<>(Arrays.asList(4,4))
+        ));
+        List<Integer> expected = new ArrayList<>(Arrays.asList(3,4));
+        assertEquals(expected, codeWorker.getSmallestArea(combinations));
+    }
+
+    @Test
+    void splitTheTextInTheColRowMatrix() {
+        var input = "haveaniceday";
+        List<Integer> rowAndCol = new ArrayList<>(Arrays.asList(3,4));
+        List<List<String>> expectedMatrix = new ArrayList<>(Arrays.asList(
+                new ArrayList<>(Arrays.asList("h","a","v","e")),
+                new ArrayList<>(Arrays.asList("a","n","i","c")),
+                new ArrayList<>(Arrays.asList("e","d","a","y"))
+        ));
+        assertEquals(expectedMatrix,codeWorker.getEncryptMatrix(input, rowAndCol));
     }
 }
