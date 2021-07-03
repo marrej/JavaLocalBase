47690d7f32a04743ddb4a0200e06ad4152f501d8
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-07-03 10:06:06 +0200

    chore(*): finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class Main {
     public static void main(String[] args)  {
@@ -10,5 +11,37 @@ public class Main {
 class CodeWorker {
     public String testMethod() {
         return "method";
+    }
+
+    public List<Integer> findClosestElements(int[] arr, Integer k, Integer x) {
+        List<Integer> kClosestElements = new ArrayList<>();
+        for (var element: arr) {
+            var hasAllActualKClosestElements = kClosestElements.size() == k;
+            if (!hasAllActualKClosestElements) {
+                kClosestElements.add(element);
+            } else {
+                var firstKCloseElement = kClosestElements.get(0);
+                var firstKCloseDistanceFromX = this.getClosenessBetween2Elements(firstKCloseElement, x);
+                var elementDistanceFromX = this.getClosenessBetween2Elements(element, x);
+                var isTheSameElement = firstKCloseElement == element;
+                var isFirstElementFromKClosestCloserThanActualElement = firstKCloseDistanceFromX <= elementDistanceFromX;
+                if (isFirstElementFromKClosestCloserThanActualElement && !isTheSameElement) {
+                    break;
+                } else {
+                    kClosestElements.remove(0);
+                    kClosestElements.add(element);
+                }
+            }
+        }
+
+        return kClosestElements;
+    }
+
+    private int getClosenessBetween2Elements (int el1, int el2) {
+        return this.getAbsolute(el1 - el2);
+    }
+
+    private int getAbsolute(int val) {
+        return val < 0 ? val * -1: val;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -15,5 +18,86 @@ class MainTest {
     @Test
     void testMethod() {
         assertEquals(codeWorker.testMethod(), "method");
+    }
+
+    @Test
+    void kClosestToNegative(){
+        var k = 3;
+        var x = -1;
+        var array = new int[]{1,2,3,4,5};
+        var result = new ArrayList<Integer>(Arrays.asList(1,2,3));
+        assertEquals(result, codeWorker.findClosestElements(array,k,x));
+    }
+
+    @Test
+    void kClosestToHighest(){
+        var k = 3;
+        var x = 6;
+        var array = new int[]{1,2,3,4,5};
+        var result = new ArrayList<Integer>(Arrays.asList(3,4,5));
+        assertEquals(result, codeWorker.findClosestElements(array,k,x));
+    }
+
+    @Test
+    void kClosestToCenter(){
+        var k = 3;
+        var x = 4;
+        var array = new int[]{1,2,3,4,5,6,7};
+        var result = new ArrayList<Integer>(Arrays.asList(3,4,5));
+        assertEquals(result, codeWorker.findClosestElements(array,k,x));
+    }
+
+    @Test
+    void kClosestToCenter1(){
+        var k = 4;
+        var x = 3;
+        var array = new int[]{1,2,3,4,5};
+        var result = new ArrayList<Integer>(Arrays.asList(1,2,3,4));
+        assertEquals(result, codeWorker.findClosestElements(array,k,x));
+    }
+
+    @Test
+    void kClosestToCenter2(){
+        var k = 1;
+        var x = 9;
+        var array = new int[]{1,1,1,10,10,10};
+        var result = new ArrayList<Integer>(Arrays.asList(10));
+        assertEquals(result, codeWorker.findClosestElements(array,k,x));
+    }
+
+    @Test
+    void kAll(){
+        var k = 3;
+        var x = 4;
+        var array = new int[]{1,2,3};
+        var result = new ArrayList<Integer>(Arrays.asList(1,2,3));
+        assertEquals(result, codeWorker.findClosestElements(array,k,x));
+    }
+
+    @Test
+    void kOne(){
+        var k = 1;
+        var x = 4;
+        var array = new int[]{1,2,3};
+        var result = new ArrayList<Integer>(Arrays.asList(3));
+        assertEquals(result, codeWorker.findClosestElements(array,k,x));
+    }
+
+    @Test
+    void kOne2(){
+        var k = 1;
+        var x = 4;
+        var array = new int[]{1,2,5};
+        var result = new ArrayList<Integer>(Arrays.asList(5));
+        assertEquals(result, codeWorker.findClosestElements(array,k,x));
+    }
+
+    @Test
+    void kOne3(){
+        var k = 1;
+        var x = 3;
+        var array = new int[]{1,2,5};
+        var result = new ArrayList<Integer>(Arrays.asList(2));
+        assertEquals(result, codeWorker.findClosestElements(array,k,x));
     }
 }
