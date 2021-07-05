6806449c428cfd21833300a5c722a022fe17d807
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-07-05 13:25:01 +0200

    chore(*): simple transpose

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JavaLocalBase
  Simple local base for java problems
+
+https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/608/week-1-july-1st-july-7th/3803/
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -15,5 +17,28 @@ class MainTest {
     @Test
     void testMethod() {
         assertEquals(codeWorker.testMethod(), "method");
+    }
+
+    @Test
+    void isIllegalOperation() {
+        int[][] originalMatrix = new int[][]{new int[]{1,2}, new int[]{3,4}};
+        var rows = 1;
+        var columns = 5;
+        assertEquals(Arrays.equals(originalMatrix, codeWorker.getTransposedMatrix(originalMatrix, rows, columns)), true);
+    }
+
+    @Test
+    void transposedToRow() {
+        int[][] originalMatrix = new int[][]{new int[]{1,2}, new int[]{3,4}};
+        var rows = 1;
+        var columns = 4;
+        int[][] trasposedMatrix = new int[][]{new int[]{1,2,3,4}};
+        var outputMatrix = codeWorker.getTransposedMatrix(originalMatrix, rows, columns);
+        for (var i=0;i<trasposedMatrix.length;i++) {
+            for (var j=0;j<trasposedMatrix[i].length;j++) {
+                System.out.println(trasposedMatrix[i][j] + "  " + outputMatrix[i][j]);
+            }
+        }
+        assertEquals(Arrays.equals(trasposedMatrix, outputMatrix), true);
     }
 }
