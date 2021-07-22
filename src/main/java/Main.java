607882e8fe3d3ae743549a61509019f4b039f3f1
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-07-22 21:55:57 +0200

    chore(*): base

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -43,27 +44,116 @@ class InputProcessor {
     Scanner inputStream = null;
     FileOutputStream outputStream = null;
     PlayerMove playerMove = null;
+    SquareMemory squareMemory = null;
+    Integer actualCoins = 0;
 
     public InputProcessor(Scanner inputStream, FileOutputStream outputStream) {
         this.inputStream = inputStream;
         this.outputStream = outputStream;
         playerMove = new PlayerMove();
+        squareMemory = new SquareMemory();
     }
 
     public FileOutputStream processAllCases() throws Exception {
-        int amountOfCases = -1;
+        int coins = -1;
         do {
             try {
-                amountOfCases = this.inputStream.nextInt();
-                System.out.println(amountOfCases);
+                coins = this.inputStream.nextInt();
+                actualCoins = coins;
+                this.prepareSquaresForCoins(actualCoins);
+                this.playGame();
                 this.outputStream.write(playerMove.getActualPlayer().getBytes());
+                this.resetPlayersMove();
             } catch (Exception e) {
                 throw new Exception("End of Stream");
             }
-        } while (amountOfCases != -1);
+        } while (coins != -1);
         return this.outputStream;
     }
 
+    private void resetPlayersMove() {
+        playerMove = new PlayerMove();
+    }
+
+    private void playGame() {
+        var isNotEndOfGame = true;
+        System.out.println("NEW GAME");
+        while (isNotEndOfGame) {
+            System.out.println(playerMove.getActualPlayer() + " " + actualCoins);
+            this.tryToReduceToZero();
+            if (actualCoins == 0) {
+                break;
+            }
+            this.tryToBlockTheOtherPlayerFromWinning();
+            System.out.println(playerMove.getActualPlayer() + " " + actualCoins);
+            playerMove.nextPlayerMove();
+        }
+    }
+
+    private void tryToReduceToZero() {
+        var allCoinsTakes = squareMemory.getAllSquaresForNumber(actualCoins);
+        if (allCoinsTakes.get(allCoinsTakes.size()-1) == actualCoins) {
+            actualCoins = 0;
+        }
+    }
+
+    private void tryToBlockTheOtherPlayerFromWinning() {
+        var breakOut = false;
+        var allCoinsTakes = squareMemory.getAllSquaresForNumber(actualCoins);
+        for (var i = allCoinsTakes.size() -1; i >=0; i-- ) {
+            var possibleBestTake = allCoinsTakes.get(i);
+            var actualRest = actualCoins - allCoinsTakes.get(i);
+            var restOfCoinsToTake = squareMemory.getAllSquaresForNumber(actualRest);
+            for (var j = restOfCoinsToTake.size() - 1; j >=0; j--) {
+                if (restOfCoinsToTake.get(j) > 0) {
+                    actualCoins -= possibleBestTake;
+                    breakOut = true;
+                    break;
+                }
+            }
+            if (breakOut) break;
+        }
+    }
+
+    private void prepareSquaresForCoins(Integer coins) {
+        squareMemory.fillAllSquaresUntilInput(coins);
+    }
+
+}
+
+class SquareMemory {
+    private TreeSet<Integer> squares = null;
+    private List<Integer> squareRoots = null;
+
+    public SquareMemory () {
+        squares = new TreeSet<>();
+        squareRoots = new ArrayList<>();
+    }
+
+    public List<Integer> getAllSquaresForNumber(Integer input) {
+        return squares.headSet(input, true).stream().collect(Collectors.toList());
+    }
+
+    public void fillAllSquaresUntilInput(Integer input) {
+        if (squareRoots.size() > 0) {
+            var isLastSquareSmallerThanInput = false;
+            do {
+                var lastSquareRoot = squareRoots.get(squareRoots.size()-1);
+                var newSquareRoot = lastSquareRoot+1;
+                squareRoots.add(newSquareRoot);
+                var newSquare= newSquareRoot*newSquareRoot;
+                squares.add(newSquare);
+                var squareList = this.getAllSquaresForNumber(input);
+                var lastSquare = squareList.get(squareList.size() - 1);
+                isLastSquareSmallerThanInput = lastSquare < input;
+                if (newSquare > input) break;
+            } while (isLastSquareSmallerThanInput);
+        } else {
+            squareRoots.add(1);
+            squares.add(1);
+        }
+
+    }
 }
 
 class PlayerMove {
