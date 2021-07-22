093ee6f33698f982647194fa3876e766d63a6726
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-07-22 20:50:43 +0200

    chore(*): base

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -41,7 +39,6 @@ class CodeWorker {
 class InputProcessor {
     Scanner inputStream = null;
     FileOutputStream outputStream = null;
-    Map<Integer, Integer> prefixSums;
 
     public InputProcessor(Scanner inputStream, FileOutputStream outputStream) {
         this.inputStream = inputStream;
@@ -50,70 +47,8 @@ class InputProcessor {
 
     public FileOutputStream processAllCases() throws IOException {
         int amountOfCases = this.inputStream.nextInt();
-        for (var i=0;i<amountOfCases;i++) findAnswerForCaseAndStoreInOuput();
-        return this.outputStream;
+        System.out.println(amountOfCases)
+;        return this.outputStream;
     }
 
-    private void findAnswerForCaseAndStoreInOuput() throws IOException {
-        initializeNewPrefixMap();
-        var answer = getAnswerForSequence();
-        storeAnswerInOutput(answer);
-    }
-
-    private Answer getAnswerForSequence() {
-        int amountOfNumbersInCase = this.inputStream.nextInt();
-        return hasPrefixSequence(amountOfNumbersInCase);
-    }
-
-    private void storeAnswerInOutput(Answer answer) throws IOException {
-        this.outputStream.write(answer.getBytes());
-    }
-
-    private Answer hasPrefixSequence(int amountOfNumbersInCase) {
-        var answer = new Answer();
-        answer.setNo();
-        int finalSum = 0;
-
-        for (var j = 0; j< amountOfNumbersInCase; j++) {
-            int nextNumber = this.inputStream.nextInt();
-            finalSum += nextNumber;
-            var differentialPrefix = prefixSums.get(finalSum);
-            if (hasAZeroSum(differentialPrefix)) answer.setYes();
-            else storeActualSumAsPrefix(finalSum);
-
-        }
-        return answer;
-    }
-
-    private boolean hasAZeroSum(Integer differentialPrefix) {
-        return differentialPrefix != null;
-    }
-
-    private Integer storeActualSumAsPrefix(int finalSum) {
-        return prefixSums.put(finalSum, 1);
-    }
-
-    private void initializeNewPrefixMap() {
-        prefixSums = new HashMap<>();
-    }
-
-
-}
-
-class Answer {
-    String yes = "yes\n";
-    String no = "no\n";
-    String finalAnswer = "";
-
-    public void setYes() {
-        finalAnswer = yes;
-    }
-
-    public void setNo() {
-        finalAnswer = no;
-    }
-
-    public byte[] getBytes() {
-        return finalAnswer.getBytes();
-    };
 }
