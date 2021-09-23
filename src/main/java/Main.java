e7a23bb0e55f78a298b340acde20efce703030e9
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-09-23 19:43:28 +0200

    chore(*): define base test case

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,4 +12,9 @@ class CodeWorker {
     public String testMethod() {
         return "method";
     }
+
+
+    public List<Deque<Integer>> solveHanoiTowers(List<Deque<Integer>> baselayout) {
+        return baselayout;
+    }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
 import java.util.Arrays;
+import java.util.Deque;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +19,34 @@ class MainTest {
     @Test
     void testMethod() {
         assertEquals(codeWorker.testMethod(), "method");
+    }
+
+    @Test
+    void moveAllBlocksToLastSpike() {
+        Deque<Integer> tower1 = new ArrayDeque<>();
+        Deque<Integer> tower2 = new ArrayDeque<>();
+        Deque<Integer> tower3 = new ArrayDeque<>();
+
+        Deque<Integer> tower12 = new ArrayDeque<>();
+        Deque<Integer> tower32 = new ArrayDeque<>();
+
+        for (var i = 1; i <= 9; i ++) tower1.add(i);
+
+        List<Deque<Integer>> baselayout = Arrays.asList(
+                tower1,
+                tower2,
+                tower3
+        );
+
+        List<Deque<Integer>> expectedLayout = Arrays.asList(
+                tower12,
+                tower2,
+                tower32
+        );
+
+        var updatedLayout = codeWorker.solveHanoiTowers(baselayout);
+
+        assertEquals(expectedLayout, updatedLayout);
+
     }
 }
