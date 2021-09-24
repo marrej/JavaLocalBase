5d5aa7fcc89c3d6718004eed5b106c3e42c1fdf6
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-09-24 14:54:15 +0200

    chore(*): base of the hanoi towers done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args)  {
@@ -9,12 +10,91 @@ public class Main {
 }
 
 class CodeWorker {
+    List<Deque<Integer>> gameLayout;
     public String testMethod() {
         return "method";
     }
 
 
-    public List<Deque<Integer>> solveHanoiTowers(List<Deque<Integer>> baselayout) {
-        return baselayout;
+    public List<Deque<Integer>> solveHanoiTowers(List<Deque<Integer>> baselayout) throws Exception {
+        this.gameLayout = baselayout;
+        var lengthOfTheHanoiTower = baselayout.get(0).getLast();
+        var isEvenAmountOfBlocksOnLastTower = lengthOfTheHanoiTower % 2 == 0;
+        generatePartialHanoiTowersUpTo(lengthOfTheHanoiTower, isEvenAmountOfBlocksOnLastTower);
+        return this.gameLayout;
+    }
+
+    private void generatePartialHanoiTowersUpTo(Integer lengthOfTheHanoiTower, boolean isEvenAmountOfBlocksOnLastTower) throws Exception {
+        for (var i = 1; i <= lengthOfTheHanoiTower; i++) {
+            var isTowerThatWeAreBuildingEven = i %2 == 0;
+            var isTowerGoingToBeOnLastSlot = isEvenAmountOfBlocksOnLastTower == isTowerThatWeAreBuildingEven;
+            this.buildTowerOfSize(i,getSlotPositionForSignificantTower(isTowerGoingToBeOnLastSlot));
+        }
+    }
+
+    public void buildTowerOfSize(Integer size, Integer inheritedSlotToBuildOn) throws Exception {
+
+            var slotTopPullFrom = findSlotContaining(size);
+            if (!slotTopPullFrom.isEmpty()) {
+                moveBlockToNewSlot(inheritedSlotToBuildOn, slotTopPullFrom);
+                moveAllPredecessorTowers(size, inheritedSlotToBuildOn);
+            } else {
+                throw new Exception();
+            }
+    }
+
+    private void moveAllPredecessorTowers(Integer size, Integer slotToBuildOn) throws Exception {
+        for (var i = 1; i < size; i ++) {
+            var deepSlotToBuildOn = getTheNextSlotToBuildSubtower(size, slotToBuildOn, i);
+            this.buildTowerOfSize(i,deepSlotToBuildOn);
+        }
+    }
+
+    private void moveBlockToNewSlot(Integer slotToBuildOn, Optional<Deque<Integer>> slotTopPullFrom) throws Exception {
+        var block = slotTopPullFrom.get().pop();
+        Deque<Integer> slotToPushTo = this.gameLayout.get(slotToBuildOn);
+        if (slotToPushTo.size() != 0 && slotToPushTo.getFirst() < block) {throw new Exception();}
+        slotToPushTo.addFirst(block);
+    }
+
+    private Integer getTheNextSlotToBuildSubtower(Integer towerBaseBlock, Integer slotToBuildOn, int actualBlock) {
+        var canActualBlockBePlacedOnTowerBase = (actualBlock % 2 == 0) == (towerBaseBlock % 2 == 0);
+        if (canActualBlockBePlacedOnTowerBase) {
+            var actualSlotForTheBlock = this.findSlotPositionContaining(actualBlock);
+            return this.getPositionWhereWeCanBuild(actualSlotForTheBlock, slotToBuildOn);
+        } else {
+            return slotToBuildOn;
+        }
+    }
+
+    private Integer findSlotPositionContaining(Integer block) {
+        for (var i=0;i<this.gameLayout.size();i++) {
+            var gameSlot = this.gameLayout.get(i);
+            if (gameSlot.size() != 0 && gameSlot.getFirst() == block) return i;
+        }
+        return 0;
+    }
+
+    public Integer getPositionWhereWeCanBuild(Integer actualNumberSlot, Integer expectedTowerSlot) {
+        List<Integer> positions = Arrays.asList(0,1,2);
+        var positionToPlace = positions.stream().filter(position -> position != actualNumberSlot && position != expectedTowerSlot).findFirst();
+        return positionToPlace.get();
+    }
+
+    private Optional<Deque<Integer>> findSlotContaining(int i) {
+        Predicate<Deque<Integer>> isSlotContainingBlock = (Deque<Integer> slot) -> slot.size() > 0 && slot.getFirst().equals(i);
+        return this.gameLayout.stream()
+                .filter(isSlotContainingBlock)
+                .findFirst();
+    }
+
+    private Integer getSlotPositionForSignificantTower(boolean isTowerGoingToBeOnLastSlot) {
+        Integer slotPosition;
+        if (isTowerGoingToBeOnLastSlot) {
+            slotPosition = 2;
+        } else {
+            slotPosition = 1;
+        }
+        return slotPosition;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -22,7 +22,22 @@ class MainTest {
     }
 
     @Test
-    void moveAllBlocksToLastSpike() {
+    void testSlotReturnal2() {
+        assertEquals(codeWorker.getPositionWhereWeCanBuild(0,1),2);
+    }
+
+    @Test
+    void testSlotReturnal0() {
+        assertEquals(codeWorker.getPositionWhereWeCanBuild(1,2),0);
+    }
+
+    @Test
+    void testSlotReturnal1() {
+        assertEquals(codeWorker.getPositionWhereWeCanBuild(0,2),1);
+    }
+
+    @Test
+    void moveAllBlocksToLastSpike() throws Exception {
         Deque<Integer> tower1 = new ArrayDeque<>();
         Deque<Integer> tower2 = new ArrayDeque<>();
         Deque<Integer> tower3 = new ArrayDeque<>();
@@ -37,6 +52,107 @@ class MainTest {
                 tower2,
                 tower3
         );
+
+
+        for (var i = 1; i <= 9; i ++) tower32.add(i);
+
+        List<Deque<Integer>> expectedLayout = Arrays.asList(
+                tower12,
+                tower2,
+                tower32
+        );
+
+        var updatedLayout = codeWorker.solveHanoiTowers(baselayout);
+
+        assertEquals(expectedLayout, updatedLayout);
+
+    }
+
+    @Test
+    void move1BlockToLastSpike() throws Exception {
+        Deque<Integer> tower1 = new ArrayDeque<>();
+        Deque<Integer> tower2 = new ArrayDeque<>();
+        Deque<Integer> tower3 = new ArrayDeque<>();
+
+        Deque<Integer> tower12 = new ArrayDeque<>();
+        Deque<Integer> tower32 = new ArrayDeque<>();
+
+        for (var i = 1; i <= 1; i ++) tower1.add(i);
+
+        List<Deque<Integer>> baselayout = Arrays.asList(
+                tower1,
+                tower2,
+                tower3
+        );
+
+
+        for (var i = 1; i <= 1; i ++) tower32.add(i);
+
+        List<Deque<Integer>> expectedLayout = Arrays.asList(
+                tower12,
+                tower2,
+                tower32
+        );
+
+        var updatedLayout = codeWorker.solveHanoiTowers(baselayout);
+
+        assertEquals(expectedLayout, updatedLayout);
+
+    }
+
+
+    @Test
+    void move2BlockToLastSpike() throws Exception {
+        Deque<Integer> tower1 = new ArrayDeque<>();
+        Deque<Integer> tower2 = new ArrayDeque<>();
+        Deque<Integer> tower3 = new ArrayDeque<>();
+
+        Deque<Integer> tower12 = new ArrayDeque<>();
+        Deque<Integer> tower32 = new ArrayDeque<>();
+
+        for (var i = 1; i <= 2; i ++) tower1.add(i);
+
+        List<Deque<Integer>> baselayout = Arrays.asList(
+                tower1,
+                tower2,
+                tower3
+        );
+
+
+        for (var i = 1; i <= 2; i ++) tower32.add(i);
+
+        List<Deque<Integer>> expectedLayout = Arrays.asList(
+                tower12,
+                tower2,
+                tower32
+        );
+
+        var updatedLayout = codeWorker.solveHanoiTowers(baselayout);
+
+        assertEquals(expectedLayout, updatedLayout);
+
+    }
+
+
+    @Test
+    void move3BlockToLastSpike() throws Exception {
+        Deque<Integer> tower1 = new ArrayDeque<>();
+        Deque<Integer> tower2 = new ArrayDeque<>();
+        Deque<Integer> tower3 = new ArrayDeque<>();
+
+        Deque<Integer> tower12 = new ArrayDeque<>();
+        Deque<Integer> tower32 = new ArrayDeque<>();
+
+        for (var i = 1; i <= 3; i ++) tower1.add(i);
+
+        List<Deque<Integer>> baselayout = Arrays.asList(
+                tower1,
+                tower2,
+                tower3
+        );
+
+
+        for (var i = 1; i <= 3; i ++) tower32.add(i);
 
         List<Deque<Integer>> expectedLayout = Arrays.asList(
                 tower12,
