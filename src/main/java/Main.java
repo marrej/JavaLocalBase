775565379d9772bc3643fb8d35a923787a91af4b
Marcus Revaj <Marcus.Revaj1@ibm.com>
2021-10-23 20:42:40 +0200

    chore(*): cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,43 +12,4 @@ class CodeWorker {
     public String testMethod() {
         return "method";
     }
-
-    public int[][] getTransposedMatrix(int[][] inputMatrix,Integer rows,Integer columns) {
-        var matrixHeight = inputMatrix.length;
-        if (matrixHeight == 0) {
-            return inputMatrix;
-        }
-        var matrixWidth = inputMatrix[0].length;
-        var originalMatrixSize = matrixHeight*matrixWidth;
-        var newMatrixSize = rows*columns;
-        var isTheNewMatrixLegal = originalMatrixSize == newMatrixSize;
-        if (!isTheNewMatrixLegal) {
-            return inputMatrix;
-        }
-
-        var deserializedMatrix = this.getDeserializedMatrix(inputMatrix);
-        var updatedMatrix = this.buildNewMatrixFromList(deserializedMatrix, rows, columns);
-        return updatedMatrix;
-    }
-
-    public int[][] buildNewMatrixFromList(List<Integer> deserializedList, Integer rows, Integer columns) {
-        var newMatrix = new int[rows][columns];
-        for (var i=0;i<rows;i++) {
-            for (var j=0;j<columns;j++) {
-                var rowAndColumnOffset = i*columns + j;
-                newMatrix[i][j] = deserializedList.get(rowAndColumnOffset);
-            }
-        }
-        return newMatrix;
-    }
-
-    private List<Integer> getDeserializedMatrix(int[][] inputMatrix) {
-        List<Integer> deserializedMatrix = new ArrayList<>();
-        for (var row: inputMatrix) {
-            for (var column: row) {
-                deserializedMatrix.add(column);
-            }
-        }
-        return deserializedMatrix;
-    }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -18,27 +18,4 @@ class MainTest {
     void testMethod() {
         assertEquals(codeWorker.testMethod(), "method");
     }
-
-    @Test
-    void isIllegalOperation() {
-        int[][] originalMatrix = new int[][]{new int[]{1,2}, new int[]{3,4}};
-        var rows = 1;
-        var columns = 5;
-        assertEquals(Arrays.equals(originalMatrix, codeWorker.getTransposedMatrix(originalMatrix, rows, columns)), true);
-    }
-
-    @Test
-    void transposedToRow() {
-        int[][] originalMatrix = new int[][]{new int[]{1,2}, new int[]{3,4}};
-        var rows = 1;
-        var columns = 4;
-        int[][] trasposedMatrix = new int[][]{new int[]{1,2,3,4}};
-        var outputMatrix = codeWorker.getTransposedMatrix(originalMatrix, rows, columns);
-        for (var i=0;i<trasposedMatrix.length;i++) {
-            for (var j=0;j<trasposedMatrix[i].length;j++) {
-                System.out.println(trasposedMatrix[i][j] + "  " + outputMatrix[i][j]);
-            }
-        }
-        assertEquals(Arrays.equals(trasposedMatrix, outputMatrix), true);
-    }
 }
