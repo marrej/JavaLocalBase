46b239877d05b1153d39fbabe665b74c0afcc0b9
Marcus <marcus.revaj@gmail.com>
2021-12-08 11:32:18 +0100

    chore(*): simplified scheduling
    n*m complexity (worstcase still 1420 * 1420) -> could make it better by doing a localization search but it would be quite hard to code it quickly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,137 @@ public class Main {
 }
 
 class CodeWorker {
+
+    private Integer startTime;
+    private Integer endTime;
+    private Integer meetingMinutes;
+    private Integer schedule2Pointer = 0;
+
     public String testMethod() {
         return "method";
+    }
+
+    public List<String> getPossibleScheduleTimes(List<List<String>> schedules, List<String> bounds, String meetingLength) {
+        var complementaryTimeBlocks = new ArrayList<String>();
+
+        var schedule1 = schedules.get(0);
+        var schedule2 = schedules.get(1);
+
+        var schedule1Free = new ArrayList<TimeRange>();
+        var schedule2Free = new ArrayList<TimeRange>();
+
+        var originalStartTime = this.translateToMinutes(bounds.get(0));
+        this.startTime = this.translateToMinutes(bounds.get(0));
+        this.endTime = this.translateToMinutes(bounds.get(1));
+        this.meetingMinutes = this.translateToMinutes(meetingLength);
+
+        if (schedule1.size() > schedule2.size()) {
+            // traverse the smaller schedule first
+            var temp = schedule2;
+            schedule2 = schedule1;
+            schedule1 = temp;
+        }
+
+        for (var i = 0; i < schedule1.size(); i++) {
+            var time = schedule1.get(i);
+            var timeRange = this.parseTimeRange(time);
+            var lastTimeBlockerToThisMeeting = (timeRange.startTime - startTime);
+            var hasFreeSpace = lastTimeBlockerToThisMeeting >= meetingMinutes;
+
+            if (hasFreeSpace) {
+                schedule1Free.add(new TimeRange(startTime, timeRange.startTime));
+            }
+            startTime = timeRange.endTime;
+
+            if (i == schedule1.size()-1) {
+                if((endTime - timeRange.endTime) >= meetingMinutes ) {
+                    schedule1Free.add(new TimeRange(timeRange.endTime, endTime));
+                }
+            }
+        }
+
+        startTime = originalStartTime;
+
+        for (var i = 0; i < schedule2.size(); i++) {
+            var time = schedule2.get(i);
+            var timeRange = this.parseTimeRange(time);
+            var lastTimeBlockerToThisMeeting = (timeRange.startTime - startTime);
+            var hasFreeSpace = lastTimeBlockerToThisMeeting >= meetingMinutes;
+
+            if (hasFreeSpace) {
+                schedule2Free.add(new TimeRange(startTime, timeRange.startTime));
+            }
+            startTime = timeRange.endTime;
+
+            if (i == schedule2.size()-1) {
+                if((endTime - timeRange.endTime) >= meetingMinutes ) {
+                    schedule2Free.add(new TimeRange(timeRange.endTime, endTime));
+                }
+            }
+        }
+
+        for (var free1: schedule1Free) {
+            for (var free2: schedule2Free) {
+                var isFree2WraparoundFree1 = free1.startTime >= free2.startTime && free1.endTime <= free2.endTime;
+                var isFree2InnerBlockOfFree1 = free1.startTime <= free2.startTime && free1.endTime >= free2.endTime;
+                var isFree2AtStartOfFree1 = free1.startTime >= free2.startTime && free2.endTime <= free1.endTime;
+                var isFree2AtTheEndOfFree1 = free1.startTime <= free2.startTime && free2.endTime >= free1.endTime;
+                if (isFree2WraparoundFree1) {
+                    complementaryTimeBlocks.add(free1.toTimeString());
+                } else if (isFree2InnerBlockOfFree1) {
+                    if ((free2.endTime - free2.startTime) >=meetingMinutes) {
+                        complementaryTimeBlocks.add(free2.toTimeString());
+                    }
+                } else if (isFree2AtStartOfFree1) {
+                    if ((free2.endTime - free1.startTime) >= meetingMinutes) {
+                        complementaryTimeBlocks.add(new TimeRange(free1.startTime, free2.endTime).toTimeString());
+                    }
+                } else if (isFree2AtTheEndOfFree1) {
+                    if ((free1.endTime - free2.startTime) >= meetingMinutes) {
+                        complementaryTimeBlocks.add(new TimeRange(free2.startTime, free1.endTime).toTimeString());
+                    }
+                }
+            }
+        }
+
+        return complementaryTimeBlocks;
+    }
+
+    private TimeRange parseTimeRange(String time) {
+        var rangeSplit = time.split("-");
+        var startTime = this.translateToMinutes(rangeSplit[0]);
+        var endTime = this.translateToMinutes(rangeSplit[1]);
+        return new TimeRange(startTime, endTime);
+    }
+
+    private Integer translateToMinutes(String time) {
+        var hoursAndMinutes = time.split(":");
+        var hours = Integer.parseInt(hoursAndMinutes[0]);
+        var minutes = Integer.parseInt(hoursAndMinutes[1]);
+
+        return hours*60 + minutes;
+    }
+}
+
+class TimeRange {
+    public Integer startTime;
+    public Integer endTime;
+
+    public TimeRange(Integer s, Integer e) {
+        this.startTime = s;
+        this.endTime = e;
+    }
+
+    public String toTimeString() {
+        return this.timeIntegerToMilitaryTime(this.startTime) + "-"+ this.timeIntegerToMilitaryTime(this.endTime);
+    }
+
+    private String timeIntegerToMilitaryTime(Integer time) {
+        var hours = time / 60;
+        var minutes = time % 60;
+
+        var hourString = hours == 0 ? "00" : hours + "";
+        var minuteString = minutes == 0 ? "00": minutes + "";
+        return hourString+":"+minuteString;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +19,28 @@ class MainTest {
     @Test
     void testMethod() {
         assertEquals(codeWorker.testMethod(), "method");
+    }
+
+
+    @Test
+    void test() {
+        List<String> schedule1 = Arrays.asList("10:00-11:30", "15:10-16:20");
+        List<String> schedule2 = Arrays.asList("8:00-9:30", "12:00-16:00", "17:20-18:00");
+        List<List<String>> schedules = Arrays.asList(schedule1, schedule2);
+        List<String> bounds = Arrays.asList("8:00","18:00");
+        String meetingLength = "0:45";
+
+        assertEquals(Arrays.asList("16:20-17:20"),codeWorker.getPossibleScheduleTimes(schedules, bounds, meetingLength));
+    }
+
+    @Test
+    void test2() {
+        List<String> schedule1 = Arrays.asList("10:15-11:30", "15:10-16:20");
+        List<String> schedule2 = Arrays.asList("8:45-9:30", "12:00-16:00", "17:10-17:15");
+        List<List<String>> schedules = Arrays.asList(schedule1, schedule2);
+        List<String> bounds = Arrays.asList("8:00","18:00");
+        String meetingLength = "0:45";
+
+        assertEquals(Arrays.asList("8:00-8:45","9:30-10:15","16:20-17:20","17:15-18:00"),codeWorker.getPossibleScheduleTimes(schedules, bounds, meetingLength));
     }
 }
