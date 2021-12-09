9c93ffe5169059a3a3ff43eb7ce27562232737cb
Marcus <marcus.revaj@gmail.com>
2021-12-09 13:20:18 +0100

    chore(*): binary search

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,57 @@ public class Main {
 }
 
 class CodeWorker {
+
+    private List<Integer> list;
+    private Integer searchValue;
+
     public String testMethod() {
         return "method";
     }
+
+    public Integer binarySearch(List<Integer> list, Integer searchingValue) {
+        this.list = list;
+        this.searchValue = searchingValue;
+//        return this.recursiveBinary(0, list.size() - 1);
+        return this.iterativeBinary();
+    }
+
+
+    private Integer iterativeBinary() {
+        var l = 0;
+        var r = this.list.size() -1;
+        while (l <= r) {
+            var mid = l + ((r - l)/2);
+            var midValue = this.list.get(mid);
+            if (midValue == this.searchValue) {
+                return midValue;
+            } else if (midValue < this.searchValue) {
+                l = mid + 1;
+            } else {
+                r = mid -1;
+            }
+        }
+
+        return null;
+    }
+
+
+
+
+
+
+    private Integer recursiveBinary(Integer left, Integer right) {
+        if (left > right) {return null;};
+        var mid = left+((right-left)/2);
+
+        var midValue = list.get(mid);
+        if (midValue == this.searchValue) {
+            return midValue;
+        } else if (midValue < this.searchValue) {
+            return this.recursiveBinary(mid + 1, right);
+        } else {
+            return this.recursiveBinary(left, mid - 1);
+        }
+    }
 }
+
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -18,4 +18,29 @@ class MainTest {
     void testMethod() {
         assertEquals(codeWorker.testMethod(), "method");
     }
+
+    @Test
+    void getLastElement() {
+        assertEquals(7,codeWorker.binarySearch(Arrays.asList(1,2,3,4,5,6,7),7));
+    }
+
+    @Test
+    void getSomeElement() {
+        assertEquals(5,codeWorker.binarySearch(Arrays.asList(1,2,3,4,5,6,7),5));
+    }
+
+    @Test
+    void getFirstElement() {
+        assertEquals(1,codeWorker.binarySearch(Arrays.asList(1,2,3,4,5,6,7),1));
+    }
+
+    @Test
+    void getNonExistentElement() {
+        assertEquals(null,codeWorker.binarySearch(Arrays.asList(1,2,3,4,5,6,7),9));
+    }
+
+    @Test
+    void getNonExistentElement2() {
+        assertEquals(null,codeWorker.binarySearch(Arrays.asList(1,2,3,4,5,6,7),-2));
+    }
 }
